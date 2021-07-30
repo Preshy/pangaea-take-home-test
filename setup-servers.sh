7bcf3d193d3743ceed5a0fd9dec54dd8525019b2
Precious Opusunju <masterpreshy1@gmail.com>
2021-07-30 12:24:16 +0100

    Update setup-servers.sh

--- setup-servers.sh
+++ setup-servers.sh
@@ -1,4 +1,4 @@
 # Start publisher server
-cd publisher-server && composer install && php artisan key:generate &
-cd subscribing-server && composer install && php artisan key:generate
+cd publisher-server && composer install && php artisan key:generate && php artisan migrate &
+cd subscribing-server && composer install && php artisan key:generate && php artisan migrate
 echo "Publisher and Subscriber server setup complete."
